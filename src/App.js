5fdf47911697e71eed931ddb9fd31d6d91be43cb
Coltyn2 <coltynwiewel@gmail.com>
2020-07-16 18:35:58 -0400

    styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useCallback, useRef } from 'react';
 import produce from 'immer';
 import './App.css';
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import Button from 'react-bootstrap/Button'
 
-let numRows = 25; // setting initial values for these varibables we'll update later
+let numRows = 20; // setting initial values for these varibables we'll update later
 let numCols = 25;
 let numPix = 25;
 let cellColor = 'gray';
@@ -12,6 +14,9 @@ let cellW = 25;
 let cellH = 25;
 let count = 0;
 let time = 200;
+let showAbout = 'none'
+let footer = `30vh`
+
 
 const operations = [ // you need this later for checking the neighbors
   [0, 1],
@@ -89,8 +94,41 @@ const runSimulation = useCallback (() => {
 
 
   return (
-    <>
-    <button onClick={() =>{
+    <div style={{
+      backgroundColor: `${bgColor}`,
+      paddingTop: `10px`,
+    }}>
+      <h1 style={{
+        color: `${borderColor}`
+      }}>
+         Coltyn's Game of Life
+         </h1>
+    <p style={{
+      color: `${borderColor}`
+    }}>Generation: {count}</p>
+    <div style={{
+      marginBottom: `2px`,
+      marginLeft: `10vw`,
+      marginRight: `10vw`,
+      display: `flex`,
+      justifyContent: `center`,
+    }}>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }} onClick={() =>{
       setRunning(!running);
       if (!running) {
       runningRef.current = true;
@@ -98,9 +136,25 @@ const runSimulation = useCallback (() => {
       runSimulation();
     } 
     }}
-    >{ running ? 'stop' : 'start'}
+    >{ running ? 'Stop' : 'Start'}
     </button>
-    <button onClick={() => {
+    
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numCols), () => (Math.random() > 0.8 ? 1 : 0)));
@@ -108,10 +162,25 @@ const runSimulation = useCallback (() => {
     }
     setGrid(rows);
     }}>
-      20% seed
+      20% Seed
     </button>
 
-    <button onClick={() => {
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numCols), () => (Math.random() > 0.6 ? 1 : 0)));
@@ -119,114 +188,50 @@ const runSimulation = useCallback (() => {
     }
     setGrid(rows);
     }}>
-      40% seed
+      40% Seed
     </button>
 
-    <button onClick={() => {
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numCols), () => (Math.random() > 0.3 ? 1 : 0)));
     }
     setGrid(rows);
     }}>
-      70% seed
+      70% Seed
     </button>
 
-    <button onClick={( ) => {
-      numRows = 25;
-      numCols = 25;
-      numPix = 25;
-      cellColor = 'gray';
-      bgColor = 'white';
-      borderColor = 'black';
-      cellW = 25;
-      cellH = 25;
-      count = 0;
-      time = 200;
-      setGrid(generateEmptyGrid());
-    }}>
-      RESET
-    </button>
-
-    <button onClick={( ) => {
-      numRows = 50;
-      numCols = 50;
-      setGrid(generateEmptyGrid());
-    }}>
-      50x50
-    </button>
-
-    <button onClick={( ) => {
-      numRows = 29;
-      numCols = 64;
-      setGrid(generateEmptyGrid());
-    }}>
-      widescreen
-    </button>
-
-    <button onClick={( ) => {
-      numRows++;
-      setGrid(generateEmptyGrid());
-    }}>
-      +row
-    </button>
-
-    <button onClick={( ) => {
-      numCols++;
-      setGrid(generateEmptyGrid());
-    }}>
-      +collum
-    </button>
-
-    <button onClick={( ) => {
-      numRows++;
-      numCols++;
-      setGrid(generateEmptyGrid());
-    }}>
-      +both
-    </button>
-
-    <button onClick={( ) => {
-      cellW++;
-      cellH++;
-      numPix = cellW
-      setGrid(generateEmptyGrid());
-    }}>
-      bigger cells
-    </button>
-
-    <button onClick={( ) => {
-      cellW--;
-      cellH--;
-      numPix = cellW
-      setGrid(generateEmptyGrid());
-    }}>
-      smaller cells
-    </button>
-
-    <button onClick={( ) => {
-       numPix++;
-      setGrid(generateEmptyGrid());
-    }}> 
-          add width space?
-    </button>   
-    
-    <button onClick={( ) => {
-      time = 50;
-      setGrid(generateEmptyGrid());
-   }}>
-     fast speed
-   </button>
-
-   <button onClick={( ) => {
-      time = 500;
-      setGrid(generateEmptyGrid());
-   }}>
-     slow speed
-   </button>
-
-
-    <button onClick={( ) => {
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }} onClick={( ) => {
       if (bgColor === 'white')
       bgColor = 'gray';
        else bgColor = 'white';
@@ -238,15 +243,334 @@ const runSimulation = useCallback (() => {
          else cellColor = 'gray';
       setGrid(generateEmptyGrid());
     }}>
-      { bgColor === 'white' ? 'darkmode' : 'lightmode'}
-    </button>
-
-    <button onClick={() => {
+          <div></div>
+      { bgColor === 'white' ? 'Darkmode' : 'Lightmode'}
+    </button>{' '}
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={() => {
     setGrid(generateEmptyGrid());
     }}>
-      clear cells
+      Clear Cells
     </button>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+    }}onClick={( ) => {
+      numRows = 20;
+      numCols = 25;
+      numPix = 25;
+      cellColor = 'gray';
+      bgColor = 'white';
+      borderColor = 'black';
+      cellW = 25;
+      cellH = 25;
+      count = 0;
+      time = 200;
+      setGrid(generateEmptyGrid());
+    }}>
+      Reset
+    </button>
+    </div>
+
+    <div style={{
+      marginBottom: `2px`,
+      marginLeft: `10vw`,
+      marginRight: `10vw`,
+      display: `flex`,
+      justifyContent: `center`,
+    }}>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numRows = 50;
+      numCols = 50;
+      setGrid(generateEmptyGrid());
+    }}>
+      50x50
+    </button>
+
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numRows = 29;
+      numCols = 64;
+      setGrid(generateEmptyGrid());
+    }}>
+      Widescreen 
+    </button>
+
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numRows++;
+      setGrid(generateEmptyGrid());
+    }}>
+      +Row
+    </button>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numRows--;
+      setGrid(generateEmptyGrid());
+    }}>
+      -Row
+    </button>
+
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numCols++;
+      setGrid(generateEmptyGrid());
+    }}>
+      +Column
+    </button>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numCols--;
+      setGrid(generateEmptyGrid());
+    }}>
+      -Column
+    </button>
+
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      numRows++;
+      numCols++;
+      setGrid(generateEmptyGrid());
+    }}>
+      +Both
+    </button>
+    </div>
+    <div style={{
+      marginBottom: `2px`,
+      marginLeft: `10vw`,
+      marginRight: `10vw`,
+      display: `flex`,
+      justifyContent: `center`,
+    }}>
+    <div></div>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }} onClick={( ) => {
+      cellW++;
+      cellH++;
+      numPix = cellW
+      setGrid(generateEmptyGrid());
+    }}>
+      Bigger cells
+    </button>
+
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      cellW--;
+      cellH--;
+      numPix = cellW
+      setGrid(generateEmptyGrid());
+    }}>
+      Smaller cells
+    </button>
+
+    {/* <button onClick={( ) => {
+       numPix++;
+      setGrid(generateEmptyGrid());
+    }}> 
+          Increase Space between Cells
+    </button>    */}
+        <div></div>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      time = 50;
+      setGrid(generateEmptyGrid());
+   }}>
+     Fast speed
+   </button>
+
+   <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginRight: `2px`,
+    }}onClick={( ) => {
+      time = 500;
+      setGrid(generateEmptyGrid());
+   }}>
+     Slow speed
+   </button>
+   </div>
+      
+
+   <div></div>
     <div 
+    class="grid"
     style ={{ 
       display: 'grid',
       gridTemplateColumns: `repeat(${numCols}, ${numPix}px)`
@@ -275,15 +599,51 @@ const runSimulation = useCallback (() => {
         ))
       }
     </div>
-    <h1>generation:{count}</h1>
-    <p> Collums: {numCols}, Rows: {numRows}, Cells are {numPix}px, </p>
+    <p style={{
+      color: `${borderColor}`
+    }}> Collums: {numCols}, Rows: {numRows}, Cells are {numPix}px </p>
+    <button style={{
+           	boxShadow: `inset 0px -3px 7px 0px ${cellColor}`,
+             background:`${cellColor}`,
+             backgroundColor:`${cellColor}`,
+             borderRadius:`3px`,
+             border:`1px solid #0b0e07`,
+             display:`inline-block`,
+             cursor:`pointer`,
+             color:`#ffffff`,
+             fontFamily:`Arial`,
+             fontSize:`15px`,
+             padding:`9px 23px`,
+             textDecoration:`none`,
+             textShadow:`0px 1px 0px #263666`,
+             marginBottom: `${footer}`,
+    }}
+    onClick={() => {
+      if (showAbout === 'none')
+      showAbout = 'contents';
+       else showAbout = 'none';
+      if (footer === '30vh')
+      footer = '0';
+       else footer = '30vh';
+       setGrid(generateEmptyGrid());
+    }}>Toggle About Section</button>
+    <div style={{
+      display: `${showAbout}`,
+      border:`solid 1px ${borderColor}`,
+      backgroundColor: `yellow`,
+      color: `${borderColor}`,
+    }}
+    >
     <h2> About Conway's Game of Life</h2>
     <p>The Game of Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
-    <p>A cell is born if</p>
+    <p>A cell is born if:</p>
     <ol>There are 3 live neighbours next to it</ol>
-    <p>A cell dies if</p>
-    <ol>It has fewer than 2 live neighbours to underpopulation, or more than 3 live neighbors to overpopulation</ol>
-    </>
+    <p>A cell dies if:</p>
+    <ol style={{
+      marginBottom:`20px`,
+    }}>It has fewer than 2 live neighbours to underpopulation, or more than 3 live neighbors to overpopulation</ol>
+    </div>
+    </div>
   );
 }
 
